095a2e1c880c5b7abf52b014431292865721e71e
kaledmhdd-tech <kaledmhdd@gmail.com>
2025-10-25 17:14:58 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# تشغيل Flask مع Gunicorn ودعم 200 نافذة متوازية
-exec gunicorn app:app --workers 4 --threads 50 --bind 0.0.0.0:$PORT
+# تشغيل Flask مع Gunicorn ودعم 50 نافذة متوازية
+exec gunicorn app:app --workers 1 --threads 50 --bind 0.0.0.0:$PORT
